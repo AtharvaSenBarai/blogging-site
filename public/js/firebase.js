d23a7d2b667a6f0356aa9c46a0744a7753c3294e
Atharva Sen <atharvasenbarai@gmail.com>
2022-01-30 15:27:08 +0530

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,5 +1,12 @@
 let firebaseConfig = {
-    // Enter your firebase credentials
+     apiKey: "AIzaSyDIdSpACQbc1-_JICLQ6gRjl_YrX25eiOA",
+  authDomain: "mini-diary-organization.firebaseapp.com",
+  databaseURL: "https://mini-diary-organization-default-rtdb.firebaseio.com",
+  projectId: "mini-diary-organization",
+  storageBucket: "mini-diary-organization.appspot.com",
+  messagingSenderId: "542075449644",
+  appId: "1:542075449644:web:26133a6448c78f79927c89",
+  measurementId: "G-26RQZMG8W6"
 };
 
 firebase.initializeApp(firebaseConfig);
